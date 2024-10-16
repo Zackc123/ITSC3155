1b0339e3119a14e06e7cec1ffa0abb7dab4c66aa
Zackc123 <160990366+Zackc123@users.noreply.github.com>
2024-10-16 19:01:52 -0400

    Assignment 4 FastAPI Implementation
    final submimssion

--- main.py
+++ main.py
@@ -1,61 +1,22 @@
-### Data ###
+from typing import Union
+from fastapi import FastAPI
+from pydantic import BaseModel
 
-recipes = {
-    "small": {
-        "ingredients": {
-            "bread": 2,  ## slice
-            "ham": 4,  ## slice
-            "cheese": 4,  ## ounces
-        },
-        "cost": 1.75,
-    },
-    "medium": {
-        "ingredients": {
-            "bread": 4,  ## slice
-            "ham": 6,  ## slice
-            "cheese": 8,  ## ounces
-        },
-        "cost": 3.25,
-    },
-    "large": {
-        "ingredients": {
-            "bread": 6,  ## slice
-            "ham": 8,  ## slice
-            "cheese": 12,  ## ounces
-        },
-        "cost": 5.5,
-    }
-}
+app = FastAPI()
 
-resources = {
-    "bread": 12,  ## slice
-    "ham": 18,  ## slice
-    "cheese": 24,  ## ounces
-}
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
 
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
-### Complete functions ###
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
 
-class SandwichMachine:
-
-    def __init__(self, machine_resources):
-        """Receives resources as input.
-           Hint: bind input variable to self variable"""
-        self.machine_resources = machine_resources
-
-    def check_resources(self, ingredients):
-        """Returns True when order can be made, False if ingredients are insufficient."""
-
-    def process_coins(self):
-        """Returns the total calculated from coins inserted.
-           Hint: include input() function here, e.g. input("how many quarters?: ")"""
-
-    def transaction_result(self, coins, cost):
-        """Return True when the payment is accepted, or False if money is insufficient.
-           Hint: use the output of process_coins() function for cost input"""
-
-    def make_sandwich(self, sandwich_size, order_ingredients):
-        """Deduct the required ingredients from the resources.
-           Hint: no output"""
-
-### Make an instance of SandwichMachine class and write the rest of the codes ###
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
